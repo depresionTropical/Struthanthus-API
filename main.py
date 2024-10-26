3aa52f19627683ecaaee1ba4e80135364aae6b0c
Hugo Martinez <le19080710@itmerida.edu.mx>
2024-10-26 16:46:36 -0600

    Refactor main.py and add image upload endpoint Add metadata

--- main.py
+++ main.py
@@ -1,12 +1,45 @@
 # main.py
+from typing import Dict, Literal
 from fastapi import FastAPI, File, UploadFile
-
+from pydantic import BaseModel
 from module.res_net import predict_class
 
-app = FastAPI()
+app = FastAPI(
+    title="Struthanthus API",
+    description="API para predecir la clase de una imagen usando ResNet.",
+    version="0.1.0",
+    contact={
+        "name": "AAAIMX",
+        "url": "https://github.com/aaaimx",
+        "email": "contact@aaaimx.org",
+    },
+    docs_url="/doc", redoc_url=None
+)
 
+class PredictionResponse(BaseModel):
+    class_predicted: Literal[0, 1]
 
-@app.post("/upload-image/")
+@app.post(
+    "/upload-image/",
+    summary="Subir imagen",
+    description="Sube una imagen y predice su clase usando ResNet.",
+    tags=["Image Classification"],
+    response_description="La clase predicha de la imagen.",
+    response_model=PredictionResponse
+)
 async def upload_image(file: UploadFile = File(...)):
+    """
+    Asynchronously uploads an image and predicts its class.
+
+    Args:
+        file (UploadFile): The image file to be uploaded.
+
+    Returns:
+        dict: A dictionary with the key 'class_predicted' and an integer value which can only be 0 or 1.
+
+    Raises:
+        ValueError: If the predicted class is not 0 or 1.
+    """
     class_predicted = predict_class.predict_image_class(file.file)
-    return { "class_predicted": class_predicted}
+
+    return {"class_predicted": int(class_predicted)}
